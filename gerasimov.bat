17fc01d7ffa3938a37d6d58cb12ecae21d76db10
nikitkakhodiev2 <proverkanedohaval@gmail.com>
2025-03-21 23:22:26 +0300

    Add files via upload

---
+++ gerasimov.bat
@@ -0,0 +1,2 @@
+@echo off
+start gerasimov.jar C:\Program Files\Eclipse Foundation\jdk-16.0.2.7-hotspot\bin\javaw.exe
---
+++ gerasimov1.bat
@@ -0,0 +1,2 @@
+@echo off
+start C:\Users\Eblan\Desktop\xui\client.jar C:\Program Files\Eclipse Foundation\jdk-16.0.2.7-hotspot\bin\javaw.exe
